3d2a6fa71b77833f60c7e77fcbbae936a8a227c5
Rahul-repositry <yadavrahul95011@gmail.com>
2024-03-23 20:54:19 +0530

    auth route started

--- api/index.js
+++ api/index.js
@@ -3,11 +3,14 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import path from "path";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); //express.json() is a middleware function that is used to parse incoming request bodies that are encoded in the JSON format. It will parse the request body and make the data available in the req.body object.
+app.use(express.urlencoded({ extended: true })); //  On the other hand, express.urlencoded() is a middleware function that is used to parse incoming request bodies that are encoded in the application/x-www-form-urlencoded format. It will also parse the request body and make the data available in the req.body object.
 
+app.use(cookieParser()); // cookie-parser is a middleware function for Express that is used to parse incoming cookies and make them available in the req.cookies object.
 mongoose
   .connect(process.env.MONGOURI)
   .then(() => {
@@ -16,6 +19,11 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+// app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+// app.use('/api/post', postRoutes);
+// app.use('/api/comment', commentRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
---
+++ api/models/user.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      validate: {
+        validator: function (value) {
+          console.log({
+            value: value,
+            typeOf: typeof value,
+          });
+          return typeof value === "string";
+        },
+        message: "Username must be a string.",
+      },
+      required: [true, " Username is Required."],
+      unique: [true, "User already present."],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is Required"],
+      unique: [true, "Email already exists"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    profilePicture: {
+      type: String,
+      default:
+        "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg",
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
